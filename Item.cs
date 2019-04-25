0056935dd8f1933572d4b20c6f40c0b65e5e68d9
Scot Finney <scotpfinney@gmail.com>
2019-04-25 11:40:03 -0400

    Item.cs
    Initial Upload

---
+++ Item.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopLibrary
+{
+    public class Item
+    {
+        public double  Price { set; get; }
+        public string Name { set; get; }
+
+        public int Quantity { set; get; }
+        public double Discount { set; get; }
+
+        public Item()            //Default constructor
+        {
+            Price = 0.0d;
+            Name = "";
+            Quantity = 0;
+            Discount = 0.0d;
+        }
+
+        public Item(double itemPrice, string itemName, int itemQuantity, double itemDiscount)            //Non-Default constructor
+        {
+            Price = itemPrice;
+            Name = itemName;
+            Quantity = itemQuantity;
+            Discount = itemDiscount;
+        }
+
+        
+        
+        public int AddInventory(int items)      //Method to add inventory to the existing quantity 
+        {
+            if (items <= 0)
+            {
+                Console.WriteLine("Inventory additions need to be <=1");
+                return 0;
+            }
+            else
+            {
+                Quantity += items;
+                return Quantity;
+            }
+        } // end AddInventory()
+    }
+}
