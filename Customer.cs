8b012f099e4601b63f2528a10875d8ffecea7386
Quran32 <quranyeamen@gmail.com>
2019-04-25 11:04:25 -0400

    adding updated customer and adding employee class.

--- Customer.cs
+++ Customer.cs
@@ -8,13 +8,45 @@ namespace MyShopLibrary
 {
     public class Customer
     {
-        int custNo;
-        string name;
+        public int CustNo { get; set; }
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int  RewardPoints { get; set; }
+        public bool SeniorCitizen  { get; set; }
+        public int Age { get; set; }
+
+
 
         public Customer()
         {
-            custNo = 1;
-            name = "Bobby";
+            this.CustNo = 0;
+            this.FirstName = " ";
+            this.Address = " ";
+            this.PhoneNumber = " ";
+            this.Email = " ";
+            this.RewardPoints = 0;
+            this.SeniorCitizen = false;
+            this.Age = 0;
+
         }
+        public Customer(int custnum , string firstName,string address ,string phoneNum, string email, int rewardPoints,
+            bool seniorCitizen, int age)
+        {
+            this.CustNo = custnum;
+            this.FirstName = firstName;
+            this.Address = address;
+            this.PhoneNumber = phoneNum;
+            this.Email = email;
+            this.RewardPoints = rewardPoints;
+            this.SeniorCitizen = seniorCitizen;
+            this.Age = age;
+
+        }
+
+    
+
+
     }
 }
---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopLibrary
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int EmployeeNumber { get; set; }
+        public int ManagerLevel  { get; set; }
+        public Employee()
+        {
+            this.Name = " ";
+            this.EmployeeNumber = 0;
+            this.ManagerLevel = 1;
+
+        }
+
+        public Employee ( string name, int empNum, int manLev)
+        {
+            this.Name = name;
+            this.EmployeeNumber = empNum;
+            this.ManagerLevel = manLev;
+        }
+
+
+    }
+}
