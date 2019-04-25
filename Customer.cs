1ec72a7e94cab6165362318c62b13f11454bcab7
Dangus94 <49986801+Dangus94@users.noreply.github.com>
2019-04-25 10:02:32 -0400

    Customer.cs Initial Upload

---
+++ Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopLibrary
+{
+    public class Customer
+    {
+        int custNo;
+        string name;
+
+        public Customer()
+        {
+            custNo = 1;
+            name = "Bobby";
+        }
+    }
+}
