9c2a76b0557e832344cbb4f59b7e1725ea2cd4e5
Dangus94 <49986801+Dangus94@users.noreply.github.com>
2019-04-26 08:54:49 -0400

    IRI.cs Adding constructors

--- ItemReceiptInstance.cs
+++ ItemReceiptInstance.cs
@@ -6,18 +6,41 @@ using System.Threading.Tasks;
 
 namespace MyShopLibrary
 {
-    class ItemReceiptInstance
+    public class ItemReceiptInstance
     {
         public string Name { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
 
+        #region Constructors
         public ItemReceiptInstance()
         {
             Name = "";
             Price = 0.0d;
             Quantity = 0;
         }
+
+        public ItemReceiptInstance(string n, double p, int q)
+        {
+            Name = n;
+            Price = p;
+            Quantity = q;
+        }
+
+        public ItemReceiptInstance(string n, double p)
+        {
+            Name = n;
+            Price = p;
+            Quantity = 0;
+        }
+
+        public ItemReceiptInstance(string n)
+        {
+            Name = n;
+            Price = 0.0d;
+            Quantity = 0;
+        }
+        #endregion
 
         public override string ToString()
         {
